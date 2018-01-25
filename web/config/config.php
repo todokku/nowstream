af05d9a6044b008e7298a4f3a2908d542931d5f1
q2293006841 <e2293006841@gmail.com>
2018-01-25 12:44:46 +0800

    Delete config.php

--- web/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyD6h-QtoVaV9VWgToYqS25IW0Jskr_B9Qw');
-define('GJ_CODE', 'HK');
-define('SITE_NAME', 'Flyue');
-define('TITLENAME', 'Flyue-Youtube');
-define('EN2DEKEY', 'aTQZ1VQ1yr');
-define('EMAIL', 'admin@godann.com');
-?>
