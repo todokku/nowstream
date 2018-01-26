bc8878aae824ef79469f096724ffc4761717b557
q2293006841 <e2293006841@gmail.com>
2018-01-26 12:43:10 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyD6h-QtoVaV9VWgToYqS25IW0Jskr_B9Qw');
-define('GJ_CODE', 'HK');
+define('GJ_CODE', 'JP');
 define('SITE_NAME', 'Flyue');
 define('TITLENAME', 'Flyue-Youtube');
 define('EN2DEKEY', 'aTQZ1VQ1yr');
